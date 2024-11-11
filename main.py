1714119e52f574a467eea68a04ad3316859123e9
julha marcolan <47335560+julhamarcolan@users.noreply.github.com>
2024-11-11 13:05:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,29 @@ Descrição: Breve descrição do que o programa faz e seu objetivo principal.
 Autores: 
   Nome do Autor (Numero USP)
 
+Este projeto faz parte do processo avaliativo da disciplina 7600105 - Física Básica I (2024) da USP-São Carlos ministrada pela(o) [Prof. Krissia de Zawadzki/Esmerindo de Sousa Bernardes]
 """
+
+OBS: usa convenções em python para definir os nomes de variáveis e de funções: 
+- snake_case para variáveis, funções e métodos;
+- PascalCase para classes;
+- SCREAMING_SNAKE_CASE para constantes.
+  
+# Abaixo segue o padrão de documentação 
+  
+def nome_da_funcao(param1, param2):
+    """
+    Descrição breve da função e o que ela realiza.
+
+    Parâmetros:
+    - param1 (tipo): Descrição do primeiro parâmetro.
+    - param2 (tipo): Descrição do segundo parâmetro.
+
+    Retorna:
+    - tipo: Descrição do valor de retorno.
+
+    Erros:
+    - TipoDeErro: Condição ou situação em que esse erro pode ocorrer.
+    """
+    # Corpo da função
+    return valor 
